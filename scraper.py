a078a18ca849185ff0b660a6f476e06a78433b31
weatherpattern <ray@weatherpattern.com>
2017-05-27 21:57:43 -0700

    Code for adding links and today's date to a database

--- scraper.py
+++ scraper.py
@@ -1,6 +1,44 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
+
+import requests
+import scraperwiki
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+# Open page
+def geturl(url):
+    try:
+        ## Access url
+        page = requests.get(url)
+        return page
+    except:
+        print ('Error accessing url')
+        return
+
+def getsouppage(page):
+    # Create beautifulsoup object from page
+    # Requests URL object -> BeautifulSoup page object
+    souppage = BeautifulSoup(page.content,"html.parser")
+    return souppage
+
+def getlinks(souppage,url):
+    # Get today's date
+    todays_date = str(datetime.now())
+    # Loop through lines of the html with an 'a' tag
+    for item in souppage.find_all('a'):
+        # Create the url, and save to the database
+        link = url + '/' + item.get('href')
+        scraperwiki.sqlite.save(unique_keys=['l'], data={"l": link, "d": todays_date })
+        print(link)
+
+if __name__ == '__main__':
+    url = 'https://www1.ncdc.noaa.gov/pub/data/15min_precip-3260'
+    page = geturl(url)
+    souppage = getsouppage(page)
+    getlinks(souppage,url)
+    
 # import scraperwiki
 # import lxml.html
 #
